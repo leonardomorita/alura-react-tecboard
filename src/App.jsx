afae58b0db55c34a2aaa3afc555baf60e7fb2531
leonardomorita <leonardomorita2@gmail.com>
2026-04-04 16:17:39 -0300

    ✨ React, JSX e componentes #1

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function FormularioDeEvento() {
+  return (
+    <form className="form-evento">
+      <h2>Preencha para criar um evento:</h2>
+
+      <fieldset>
+        <label htmlFor="nome">Qual é o nome do evento?</label>
+        <input type="text" id="nome" />
+      </fieldset>
+    </form>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="Logo da empresa" />
+      </header>
+
+      <section>
+        <img src="/banner.png" alt="Banner" />
+      </section>
+
+      <FormularioDeEvento />
+    </main>
+  )
+}
+
+export default App
